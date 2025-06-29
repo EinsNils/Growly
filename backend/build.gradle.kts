edd8d9d555b07316ebac1494b4f2a195922482cf
Nils Zentgraf <nils.zentgraf@aol.com>
2025-06-29 19:51:04 +0200

    feat: Integrate OpenAPI support and update security configuration for API documentation

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("com.microsoft.sqlserver:mssql-jdbc:12.8.1.jre11")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
 
 
